ce119c315fc440dd4b6ffd6f86445f8ecebf3c71
romezer <romezer@gmail.com>
2020-01-17 12:49:46 +0200

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '657001902405-fcce53siejr6tlbhbu1p72s6co9hbpr9.apps.googleusercontent.com',
-  googleClientSecret: 'Y8XXCsdsPjOhznjuNdIhQ6pM',
-  mongoURI: 'mongodb://rom:rom9891252@ds249839.mlab.com:49839/blogger_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
